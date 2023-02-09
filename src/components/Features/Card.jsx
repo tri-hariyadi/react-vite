a076eb4ca6729059c36342b9806e9f8ad77d0c2f
tri hariyadi <trihariyadi24@gmail.com>
2023-02-09 14:09:19 +0700

    Add features section

--- src/components/Features/Card.jsx
+++ src/components/Features/Card.jsx
@@ -1,42 +1,45 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({data, idx}) => {
+  const getBgColor = () => {
+    if (data.isActive) {
+      return 'bg-orange-400';
+    }
+    return 'bg-[#757575]';
+  };
+
+  const getBorderColor = () => {
+    if (data.isActive) {
+      return 'border-[#13A458]';
+    }
+    return 'border-[#C4C4C4]';
+  };
+
+  const alignment = () => {
+    if (idx % 2 !== 0) {
+      return 'mr-auto';
+    }
+    return 'ml-auto';
+  };
+
   return (
-    <div className='w-1/2 p-4 flex-row'>
-      {/* <div className='flex flex-row shadow-[0_4px_20px_3px_rgba(0,0,0,0.15)]
-        rounded-[20px] justify-center items-center h-full'>
-        <div className='flex items-center justify-center border-4 w-[110px] h-[110px]
-          border-[#13A458] bg-[#FEFEFE] -ml-16 rounded-[50%]'>
-          <img src='https://kotakode.com/static/media/landingGraphic4.1ed65e42.svg'
-            className='w-[238px] h-[64px] m-auto object-center' />
-        </div>
-        <div className='p-6 pt-0'>
-          <h1 className='text-left text-[#272727] text-[24px] pt-[0.67rem] font-bold'>Online Forum</h1>
-          <p className='my-[0.65rem] text-[#272727] text-[16px]'>
-            Kotakode dapat menjawab pertanyaan dan membantu programmer dalam memberikan jawaban yang berkualitas
-          </p>
-          <div className='flex justify-center'>
-            <button
-              className='bg-orange-400 px-[16px] py-[8px] text-[16px] leading-[32px] font-bold text-white rounded-md'>
-              Tanya Sekarang
-            </button>
-          </div>
-        </div>
-      </div> */}
+    <div className={['w-1/2 m-4', alignment()].join(' ')}>
       <div className='flex flex-row h-full items-center justify-center shadow-[0_4px_20px_3px_rgba(0,0,0,0.15)] rounded-[20px]'>
-        <div className='w-[130px] h-[110px] border-4 bg-[#FEFEFE] border-[#13A458] flex items-center justify-center -ml-16 rounded-[50%]'>
-          <img src='https://kotakode.com/static/media/landingGraphic4.1ed65e42.svg'
+        <div className={['w-[130px] h-[110px] border-4 bg-[#FEFEFE] flex items-center justify-center -ml-14 rounded-[50%]', getBorderColor()].join(' ')}>
+          <img src={data.imgUrl}
             className='w-[238px] h-[64px] m-auto object-center' />
         </div>
         <div className='p-6 pt-0 w-full'>
-          <h1 className='text-left text-[#272727] text-[24px] pt-[0.67rem] font-bold'>Online Forum</h1>
-          <p className='my-[0.65rem] text-[#272727] text-[16px]'>
-            Kotakode dapat menjawab pertanyaan dan membantu programmer dalam memberikan jawaban yang berkualitas
+          <h1 className='text-left text-[#272727] text-[24px] pt-[0.67rem] font-bold'>
+            {data.title}
+          </h1>
+          <p className='my-[0.65rem] text-[#272727] text-[14px]'>
+            {data.desc}
           </p>
           <div className='flex justify-center'>
             <button
-              className='bg-orange-400 px-[16px] py-[8px] text-[16px] leading-[32px] font-bold text-white rounded-md'>
-              Tanya Sekarang
+              className={['flex-1 px-[16px] py-[8px] text-[16px] leading-[32px] font-bold text-white rounded-md', getBgColor()].join(' ')}>
+              {data.textBtn}
             </button>
           </div>
         </div>
--- src/components/Features/index.jsx
+++ src/components/Features/index.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Card from './Card';
 
 const Features = () => {
+  const [data] = useState([
+    {
+      title: 'Online Forum',
+      desc: 'Kotakode dapat menjawab pertanyaan dan membantu programmer dalam memberikan jawaban yang berkualitas',
+      imgUrl: 'https://kotakode.com/static/media/landingGraphic4.1ed65e42.svg',
+      textBtn: 'Tanya Sekarang',
+      isActive: true,
+    },
+    {
+      title: 'Microblogging',
+      desc: 'Kotakode menyediakan wadah bagi programmer untuk berbagi wawasan terkait info terkini di bidang IT',
+      imgUrl: 'https://kotakode.com/static/media/landingGraphic5.16f83186.svg',
+      textBtn: 'Tulis Sekarang',
+      isActive: true,
+    },
+    {
+      title: 'Job Hiring (Coming Soon)',
+      desc: 'Kotakode membantu rekruiter dalam mencari talent terbaik yang memenuhi standar perusahaan',
+      imgUrl: 'http://localhost:5173/feature3.png ',
+      textBtn: 'Cari Kerja',
+      isActive: false,
+    },
+  ]);
+
   return (
     <div className='mt-24 mb-24 flex flex-row items-center'>
       <div className='w-1/2 featureleft items-start'>
@@ -16,7 +40,10 @@ const Features = () => {
         </p>
       </div>
       <div className='w-1/2 items-end flex-col -m-4 flex flex-wrap justify-center'>
-        <Card />
+        {data.map((item, idx) => (
+          <Card key={`feature-${idx}`} data={item}
+            idx={idx} />
+        ))}
       </div>
     </div>
   );
